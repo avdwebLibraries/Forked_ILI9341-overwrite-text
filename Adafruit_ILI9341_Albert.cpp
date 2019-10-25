55c16fa142d571b67457eb57151fd6c5fc4e27c9
Albert van Dalen <albertfora@avdweb.nl>
2019-10-25 11:30:37 +0200

    Add files via upload

--- Adafruit_ILI9341_Albert.cpp
+++ Adafruit_ILI9341_Albert.cpp
@@ -7,6 +7,7 @@ With this library extension we can overwrite text and numbers correctly.
 info print functions: https://github.com/esp8266/Arduino/blob/master/cores/esp8266/Print.h
 
 7-10-2019 version v1.0.0
+20-10-2019 version v1.0.1 also for float
 */
 
 #include "Adafruit_ILI9341_Albert.h"
@@ -17,6 +18,18 @@ Adafruit_ILI9341(cs, dc, rst)
 }
 
 void Adafruit_ILI9341_Albert::printNew(const long value, const int chCount)
+{ getNumberBounds(chCount);
+  print(value); 
+  setCursor(OriginalCursor_x, OriginalCursor_y);  
+}
+
+void Adafruit_ILI9341_Albert::printNew(const float value, const unsigned decimals, const int chCount)
+{ getNumberBounds(chCount);
+  print(value, decimals); 
+  setCursor(OriginalCursor_x, OriginalCursor_y);  
+}
+
+void Adafruit_ILI9341_Albert::getNumberBounds(const int chCount)
 { OriginalCursor_x = cursor_x; 
   OriginalCursor_y = cursor_y;
   getTextBounds("3",  OriginalCursor_x, OriginalCursor_y, &x, &y, &charWidth, &h); // THIS CODE IS UGLY 
@@ -25,9 +38,7 @@ void Adafruit_ILI9341_Albert::printNew(const long value, const int chCount)
   w = chCount*charWidth + (chCount-1)*space;
   // Serial << x, y, w, h; // 0: 22 64 22 34 // 00: 22 64 48 34
   // textbgcolor = ILI9341_GREEN; // testing with green background
-  fillRect(x, y, w, h, textbgcolor); // textbgcolor is protected in Adafruit_GFX.h
-  print(value); 
-  setCursor(OriginalCursor_x, OriginalCursor_y);  
+  fillRect(x, y, w, h, textbgcolor); // textbgcolor is protected in Adafruit_GFX.h 
 }
 
 void Adafruit_ILI9341_Albert::printNew(const String &newString, const String &oldString) // overloading needs const here
--- Adafruit_ILI9341_Albert.h
+++ Adafruit_ILI9341_Albert.h
@@ -8,8 +8,11 @@ class Adafruit_ILI9341_Albert: public Adafruit_ILI9341
 public: 
   Adafruit_ILI9341_Albert(int8_t cs, int8_t dc, int8_t rst);
   void printNew(const long value, const int chCount);
+  void printNew(const float value, const unsigned decimals, const int chCount);
   void printNew(const String &newString, const String &oldString); 
+
 protected:
+  void getNumberBounds(const int chCount);
   int16_t x, y, OriginalCursor_x, OriginalCursor_y;
   uint16_t w, h, charWidth, char2Width;
 };
